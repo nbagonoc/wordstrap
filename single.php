63105c38c4577a9b84c6b7c1d2a1f4f36a8195a7
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-09-18 20:49:49 +0800

    added image to single.php

--- single.php
+++ single.php
@@ -6,8 +6,10 @@
 				<!-- content -->
 				<div class="col-md-8 mb-3 pr-md-1">
 					<div class="card">
+						<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+						<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'background' ); ?>
+						<div class="card-img-top" style="background-image: url('<?php echo $thumb['0'];?>')"></div>
 						<div class="card-body">
-							<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 							<h2 class="text-capitalize text-red"><?php the_title(); ?></h2>
 							<?php the_content(); ?>
 							<?php endwhile; ?>
