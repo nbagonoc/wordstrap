3e6cffbaed17f6a530cec5e832818429428fc178
Niccu Bagonoc ~Coy <nicbag@jpj.com>
2020-12-20 18:35:27 +0800

    Added image support

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,29 @@ const extract = require('mini-css-extract-plugin');
 module.exports = {
     entry: './app.js',
     output: {
-        path: path.resolve(__dirname, 'css')
+        path: path.resolve(__dirname, 'css'),
+        publicPath: ''
     },
-    plugins: [new extract({filename: "custom.css"})],
+    plugins: [
+        new extract({filename: 'custom.css'})
+    ],
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                extract.loader,
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    extract.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: '../img',
+                },
+            }
+        ]
     }
 }
